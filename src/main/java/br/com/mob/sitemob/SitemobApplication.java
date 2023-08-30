92d006d07313c5b845ee84c22ebc8f5392b31d01
Paola Martins <paola.nmartins@gmail.com>
2023-08-29 21:00:32 -0300

    atua

---
+++ src/main/java/br/com/mob/sitemob/SitemobApplication.java
@@ -0,0 +1,13 @@
+package br.com.mob.sitemob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mob/sitemob/SitemobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mob.sitemob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
